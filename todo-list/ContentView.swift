10a0bbc974e93fe5e4d915ce70037e472d84f3de
rbpolim <rbpolim@gmail.com>
2024-01-14 16:56:04 -0300

    feat: simple todo list done

--- todo-list/ContentView.swift
+++ todo-list/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var txt: String = ""
+    @State var toDos: [String] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                ZStack(alignment: .trailing) {
+                    TextField(
+                        "Item",
+                        text: $txt
+                    )
+                        .padding()
+                        .frame(height: 60)
+                        .background(Color.gray)
+                        .cornerRadius(8)
+                        
+                    RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
+                        .frame(width: 80, height: 60)
+                        .foregroundColor(.blue)
+                        .overlay(
+                            Text("Add")
+                                .foregroundColor(.white)
+                                .font(.title3)
+                        )
+                        .onTapGesture {
+                            if (txt.count > 0) {
+                                toDos.append(txt)
+                                txt = ""
+                            }
+                        }
+                }
+                
+                List {
+                    ForEach(toDos, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
